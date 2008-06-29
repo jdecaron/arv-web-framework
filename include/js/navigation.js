83e6e6096494cb3706d0c18194222acb332ee347
Jean-Denis Caron <jdecaron@gmail.com>
2008-06-29 00:31:53 -0400

    modified:   include/js/navigation.js

--- include/js/navigation.js
+++ include/js/navigation.js
@@ -5,15 +5,42 @@ function userLinkClick(url, type){
     // Process the URL with the AJAX
     // page loading system.
     if(type == 'ajax'){
-        url_array = url.split('&')
+        var url_array = url.split('&')
 
         // Start the loading indacator for
         // the users.
         loadUrlInElementId('http://192.168.1.102/find-spots.com/include/tpl/test/page2.php', 'a0_b1');
 
-        template_xml = xmlDOM(template.template0());
-        getUrlListFromTemplate(template_xml.firstChild.firstChild);
+        var template_xml = xmlDOM(template.template0());
+        var template1_xml = xmlDOM(template.template1());
+        // Retrieve all the URLs of a template and put them in
+        // an associative array.
+        window.actualUrlList_array = Array();
+        processTemplateStructure(template_xml.firstChild.firstChild, 'actual');
+        window.nextUrlList_array = [];
+        processTemplateStructure(template1_xml.firstChild.firstChild, 'next');
+        loadBlocks();
     }
+}
+
+function loadBlocks(){
+    urlsToLoad = [];
+    for(var i=0;i<window.nextUrlList_array.length;i++){
+        if(window.actualUrlList_array[window.nextUrlList_array[i][1]] != null && isNaN(window.nextUrlList_array[i][1])){
+            // Get the content of the blocks which
+            // they are already loaded in the page and
+            // save it in the array which will be used
+            // to show the next page.
+            window.nextUrlList_array[i][1] = document.getElementById(window.actualUrlList_array[window.nextUrlList_array[i][1]]).innerHTML;
+        }else{
+            // Set a list of URLs to load asynchronously.
+            // The first cell is the index to know in which
+            // cell of the next URL list the content has 
+            // to be placed in.
+            urlsToLoad[urlsToLoad.length] = [i, window.nextUrlList_array[i][1]];
+        }
+    }
+alert(window.nextUrlList_array);
 }
 
 // Array that store specific information which is related to that
@@ -30,11 +57,19 @@ function loadContentInElementId(response, url){
     document.getElementById(requestedUrlToLoadIn_array[url]).innerHTML = response;
 }
 
-function getUrlListFromTemplate(childs_xml){
+function processTemplateStructure(childs_xml, action){
+    // Process the nodes "childs" of this node.
     for(var i=0;i<childs_xml.childNodes.length;i++){
         for(var j=0;j<childs_xml.childNodes[i].childNodes.length;j++){
             if(childs_xml.childNodes[i].childNodes[j].nodeName == 'childs'){
-                getUrlListFromTemplate(childs_xml.childNodes[i].childNodes[j]);
+                processTemplateStructure(childs_xml.childNodes[i].childNodes[j], action);
+            }
+
+            if(childs_xml.childNodes[i].childNodes[j].nodeName == 'load' && action == 'actual'){
+                window.actualUrlList_array[childs_xml.childNodes[i].childNodes[j].firstChild.nodeValue] = childs_xml.childNodes[i].nodeName;
+            }
+            if(childs_xml.childNodes[i].childNodes[j].nodeName == 'load' && action == 'next'){
+                window.nextUrlList_array[window.nextUrlList_array.length] = [childs_xml.childNodes[i].nodeName, childs_xml.childNodes[i].childNodes[j].firstChild.nodeValue];
             }
         }
     }
