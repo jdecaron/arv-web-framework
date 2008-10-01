7752e47aff2e87a854b4824b8368c74325148f8b
Jean-Denis Caron <jdecaron@gmail.com>
2008-10-01 06:47:03 -0400

    new file:   include/tpl/arv/advertisement.php 	new file:   include/tpl/arv/screencast.php

---
+++ include/tpl/arv/advertisement.php
@@ -0,0 +1,4 @@
+<div style="background-color:white;width:300px;">
+<div style="margin-top:35px;font-size:18px;color:#547DA1;">Advertisement</div>
+<div><img width="279" height="229" src="img/arv/ad<?=rand(1,4);?>.gif"></div>
+</div>
---
+++ include/tpl/arv/screencast.php
@@ -0,0 +1,4 @@
+<div style="background-color:white;width:300px;">
+<div style="margin-top:35px;font-size:18px;color:#547DA1;">Screencast</div>
+<div><a href="#"><img width="279" height="186" src="img/arv/screencast.gif" style="border-style:none;"></a></div>
+</div>
